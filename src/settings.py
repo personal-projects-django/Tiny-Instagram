06b92823c8a2976d944183b4839698607eda1d9d
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-02-15 22:58:07 +0330

    feat:writing apps names in setting

--- src/settings.py
+++ src/settings.py
@@ -39,7 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'information.apps.InformationConfig',
+    'posts.apps.PostsConfig',
+    'notifications.apps.NotificationsConfig',
+    'story.apps.StoryConfig',
     'rest_framework',
+
 ]
 
 MIDDLEWARE = [
