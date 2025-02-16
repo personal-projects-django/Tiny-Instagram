9dcd46cbf1b0220dff90653a6e5d1d33c022a09b
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-02-16 14:56:23 +0330

    FEAT(template):Add files password,base,header,home.html template

--- account/serializers.py
+++ account/serializers.py
@@ -1,3 +1,29 @@
 from rest_framework import serializers
 
 from account.models import User
+
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True,required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email','password','password2']
+        extra_kwargs = {'password':{'write_only':True}}
+
+
+    def create(self, validated_data):
+        del validated_data['password2']
+        return User.objects.create_user(**validated_data)
+
+
+    def validate_username(self, value):
+        if value == 'password':
+            raise serializers.ValidationError('username can not be password')
+        return value
+
+    def validate_password(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('passwords must match')
+        return data
--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserRegisterSerializer
 # Create your views here.
+
+
+class UserRegisterView(APIView):
+    serializer_class = UserRegisterSerializer
+    def post(self, request):
