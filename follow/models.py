e895dd043be6a05333bc4feec04f0c84f9e5ac22
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-02-25 02:34:07 +0330

    FEAT(details):creating and start model of follow

--- follow/models.py
+++ follow/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from account.models import User
 # Create your models here.
+
+
+
+class Friendship(models.Model):
+    follow_from = models.ForeignKey(User, on_delete=models.PROTECT, related_name='friend_follow_from')
+    follow_to = models.ForeignKey(User, on_delete=models.PROTECT, related_name='friend_follow_to')
+    is_accepted = models.BooleanField(default=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('follow_from', 'follow_to')
--- follow/urls.py
+++ follow/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
-
-
-
-
-]
+# urlpatterns = [
+#     path('users-list/', views.UserListView.as_view()),
+#     path('request/', views.RequestView.as_view()),
+#     path('requests-list/', views.RequestListView.as_view()),
+#     path('accept/', views.AcceptView.as_view()),
+#     path('friends/', views.FriendListView.as_view())
+#
+#
+#
+# ]
 
 
 
