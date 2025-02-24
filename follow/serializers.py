693c8aeabcf4571e9a1f306cab406c008455c869
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-02-25 02:34:41 +0330

    FEAT(details):creating and start serializer

--- follow/serializers.py
+++ follow/serializers.py
@@ -1,15 +1,15 @@
-# from rest_framework import serializers
-# from account.models import User
-#
-#
-# class UserListSerializer(serializers.ModelSerializer):
-#     avatar = serializers.SerializerMethodField()
-#
-#     class Meta:
-#         model = User
-#         fields = ('id', 'username', 'avatar')
-#
-#     def get_avatar(self, instance):
-#         if hasattr(instance, 'profile') and instance.profile.avatar:
-#             return instance.profile.avatar.url
-#         return ''
+from rest_framework import serializers
+from account.models import User
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    avatar = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'avatar')
+
+    def get_avatar(self, instance):
+        if hasattr(instance, 'profile') and instance.profile.avatar:
+            return instance.profile.avatar.url
+        return ''
