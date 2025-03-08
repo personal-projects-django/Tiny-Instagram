d3651499400a82ea73b35b2e7dfc45ca5dce60b8
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-08 17:28:32 +0330

    FEAT(details):creating and start model in admin panel

--- follow/admin.py
+++ follow/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from follow.models import Friendship
+
+
+@admin.register(Friendship)
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ['follow_from', 'follow_to', 'is_accepted', 'created_time']
+    actions = False
+
+    # def has_add_permission(self, request):
+    #     return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Comment, Like
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('caption', 'user', 'is_active', 'created_at')
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'is_approved')
+    list_filter = ('is_approved',)
+    date_hierarchy = 'created_at'
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'is_liked')
+    list_filter = ('is_liked',)
+    date_hierarchy = 'created_at'
